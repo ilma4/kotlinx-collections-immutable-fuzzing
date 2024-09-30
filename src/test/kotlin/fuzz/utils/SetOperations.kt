05ca63af031256aeb72a2cc6477169b8be0d73fd
Ilia Malakhov <ilya.malakhov4@gmail.com>
2024-09-30 12:00:30 +0200

    Set targets fixed

--- src/test/kotlin/fuzz/utils/SetOperations.kt
+++ src/test/kotlin/fuzz/utils/SetOperations.kt
@@ -1,16 +1,18 @@
 package fuzz.utils
 
 import com.code_intelligence.jazzer.api.FuzzedDataProvider
+import kotlin.reflect.full.isSubclassOf
 
 private val setOperations = SetOperation::class.sealedSubclasses
-private val emptySetOperations = setOperations.filter{ it is EmptyOperation}
+private val emptySetOperations = setOperations.filter { it.isSubclassOf(EmptyOperation::class) }
 
-fun FuzzedDataProvider.consumeSetOperation(set: kotlin.collections.Set<Int>): SetOperation{
+fun FuzzedDataProvider.consumeSetOperation(set: kotlin.collections.Set<Int>): SetOperation {
     val op = if (set.isEmpty()) pickValue(emptySetOperations) else pickValue(setOperations)
 
-    return when(op){
+    return when (op) {
         Add::class -> Add(consumeInt())
         Remove::class -> Remove(consumeInt())
+        AddAll::class -> AddAll(consumeInts(10).toSet())
         else -> throw IllegalStateException("Unexpected operation: $op")
     }
 }
